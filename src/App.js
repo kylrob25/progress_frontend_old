7277d6a824aa0aec6a7b6a4be99d4d5c55f504c4
Kyle Roberts <kylrob25@icloud.com>
2024-02-11 15:24:59 +0000

    Mess around with fetch api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [trainers, setTrainers] = useState([]);
+
+    const fetchTrainers = async() => {
+        const response = await fetch('http://localhost:8080/api/trainers')
+        const data = await response.json()
+        setTrainers(data)
+    }
+
+    useEffect(() => {
+        fetchTrainers().catch((err) => {
+            console.log(err.message)
+        })
+    }, []);
+
+    return (
+        <div className="trainers">
+            <table>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Forename</th>
+                    <th>Surname</th>
+                    <th>Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                {trainers.map((trainer) => (
+                    <tr key={trainer.id}>
+                        <td>{trainer.id}</td>
+                        <td>{trainer.forename}</td>
+                        <td>{trainer.surname}</td>
+                        <td>{trainer.email}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
