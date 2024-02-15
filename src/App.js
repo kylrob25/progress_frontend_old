320029388d8958817c9e360ec0522af6889cad58
Kyle Roberts <kylrob25@icloud.com>
2024-02-15 13:11:01 +0000

    Try and figure out routing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
-import './App.css';
-import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import ViewClients from "./pages/ViewClients";
+import NoPage from "./pages/NoPage";
 
 const App = () => {
-    const [trainers, setTrainers] = useState([]);
-
-    const fetchTrainers = async() => {
-        const response = await fetch('http://localhost:8080/api/trainers')
-        const data = await response.json()
-        setTrainers(data)
-    }
-
-    useEffect(() => {
-        fetchTrainers().catch((err) => {
-            console.log(err.message)
-        })
-    }, []);
-
     return (
-        <div className="trainers">
-            <table>
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Forename</th>
-                    <th>Surname</th>
-                    <th>Email</th>
-                </tr>
-                </thead>
-                <tbody>
-                {trainers.map((trainer) => (
-                    <tr key={trainer.id}>
-                        <td>{trainer.id}</td>
-                        <td>{trainer.forename}</td>
-                        <td>{trainer.surname}</td>
-                        <td>{trainer.email}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route path="/clients" element={<ViewClients />} />
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/NoPage.js
@@ -0,0 +1,7 @@
+const NoPage = () => {
+    return (
+        <h1>Error: Page not found!</h1>
+    )
+}
+
+export default NoPage
