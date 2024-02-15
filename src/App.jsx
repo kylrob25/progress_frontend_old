7305643aaa8a7bc5e1a75b0c6bb07ced2167ed32
Kyle Roberts <kylrob25@icloud.com>
2024-02-15 13:28:32 +0000

    Mess around with routing some more

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import ViewClients from "./pages/ViewClients";
 import NoPage from "./pages/NoPage";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/">
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
                     <Route path="/clients" element={<ViewClients />} />
                     <Route path="*" element={<NoPage />} />
                 </Route>
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,12 @@
+import {Outlet, Link} from "react-router-dom"
+
+const Layout = () => {
+    return (
+        <>
+            <h1>TODO: nav</h1>
+            <Outlet/>
+        </>
+    )
+}
+
+export default Layout
