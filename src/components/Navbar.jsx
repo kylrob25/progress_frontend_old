5392179fbf01a3d59b18aae6849159d6776dfa39
Kyle Roberts <kylrob25@icloud.com>
2024-02-20 07:32:02 +0000

    Fix navbar on smaller screens

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const NavbarComponent = () => {
+    const [open, setOpen] = useState(false)
+
+    const handleToggle = () => {
+        setOpen(!open)
+    }
+
+    return (
+        <Navbar bg="dark" variant="dark" expand="md">
+            <Container>
+                <Navbar.Brand href="#home">Progress</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle}/>
+                <Navbar.Collapse id="navbar-collapse">
+                    <Nav className="ml-auto" style={{flex: 1}}>
+                        <Link to="/" className="nav-link">Home</Link>
+                    </Nav>
+                    <Nav className="justify-content-end" style={{flex: 1}}>
+                        <Link to="/login" className="nav-link">Login</Link>
+                        <Link to="/register" className="nav-link">Register</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavbarComponent;
--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,9 +1,35 @@
-import React from "react"
+import {Button, Form} from "react-bootstrap"
+import ReCAPTCHA from "react-google-recaptcha"
+import {useState} from "react";
 
 const RegistrationForm = () => {
+
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [recaptchaValue, setRecaptchaValue] = useState()
+
+    const handleSubmit = (e) => {
+
+    }
+
     return (
-        <>
-        </>
+        <Form>
+            <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </Form.Group>
+
+            <ReCAPTCHA sitekey="6Le9bncpAAAAALuqa1X7f6v2GlzXsnzTT4TSpmfE" onChange={(value) => setRecaptchaValue(value)}/>
+
+            <br/>
+
+            <Button variant="primary" type="submit" /* disabled={!recaptchaValue} */>Submit</Button>
+        </Form>
     )
 }
 
