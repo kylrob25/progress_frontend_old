f97eda9ac5e67165bc26097964ca6588c90e5796
Kyle Roberts <kylrob25@icloud.com>
2024-02-22 04:39:01 +0000

    Stick to the same card theme throughout

--- src/pages/user/CreateUser.jsx
+++ src/pages/user/CreateUser.jsx
@@ -5,7 +5,7 @@ import {
     Container,
     Typography,
     TextField,
-    Button, MenuItem, Checkbox, ListItemText, Select, InputLabel, FormControl,
+    Button, MenuItem, Checkbox, ListItemText, Select, InputLabel, FormControl, Grid, Card, CardContent,
 } from "@material-ui/core";
 import { useForm } from "react-hook-form";
 
@@ -47,80 +47,87 @@ const CreateUser = () => {
     }, []);
 
     return (
-        <Container maxWidth="sm">
-            <Typography variant="h5" component="h1" gutterBottom>
-                Create User
-            </Typography>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <TextField
-                    {...register("username", { required: true })}
-                    label="Username"
-                    variant="outlined"
-                    error={errors.username ? true : false}
-                    helperText={errors.forename ? 'Username is required' : ''}
-                    fullWidth
-                    margin="normal"
-                />
-                <TextField
-                    {...register("forename", { required: true })}
-                    label="Forename"
-                    variant="outlined"
-                    error={errors.forename ? true : false}
-                    helperText={errors.forename ? 'Forename is required' : ''}
-                    fullWidth
-                    margin="normal"
-                />
-                <TextField
-                    {...register("surname", { required: true })}
-                    label="Surname"
-                    variant="outlined"
-                    error={errors.surname ? true : false}
-                    helperText={errors.surname ? 'Surname is required' : ''}
-                    fullWidth
-                    margin="normal"
-                />
-                <TextField
-                    {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-                    label="Email"
-                    variant="outlined"
-                    type="email"
-                    error={errors.email ? true : false}
-                    helperText={errors.email ? 'Invalid email address' : ''}
-                    fullWidth
-                    margin="normal"
-                />
 
-                <FormControl fullWidth margin="normal">
-                    <InputLabel id="role-select-label">Roles</InputLabel>
-                    <Select
-                        labelId="role-select-label"
-                        multiple
-                        value={selectedRoles}
-                        onChange={handleRoleChange}
-                        renderValue={(selected) => selected.join(', ')}
-                        MenuProps={{
-                            PaperProps: {
-                                style: {
-                                    maxHeight: 224,
-                                    width: 250,
-                                },
-                            },
-                        }}
-                    >
-                        {allRoles.map((role) => (
-                            <MenuItem key={role} value={role}>
-                                <Checkbox checked={selectedRoles.indexOf(role) > -1} />
-                                <ListItemText primary={role} />
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
+        <Grid container justifyContent="center">
+            <Grid item xs={12} sm={8} md={6} lg={4}>
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" component="h1" gutterBottom>
+                            Create User
+                        </Typography>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <TextField
+                                {...register("username", { required: true })}
+                                label="Username"
+                                variant="outlined"
+                                error={errors.username ? true : false}
+                                helperText={errors.forename ? 'Username is required' : ''}
+                                fullWidth
+                                margin="normal"
+                            />
+                            <TextField
+                                {...register("forename", { required: true })}
+                                label="Forename"
+                                variant="outlined"
+                                error={errors.forename ? true : false}
+                                helperText={errors.forename ? 'Forename is required' : ''}
+                                fullWidth
+                                margin="normal"
+                            />
+                            <TextField
+                                {...register("surname", { required: true })}
+                                label="Surname"
+                                variant="outlined"
+                                error={errors.surname ? true : false}
+                                helperText={errors.surname ? 'Surname is required' : ''}
+                                fullWidth
+                                margin="normal"
+                            />
+                            <TextField
+                                {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+                                label="Email"
+                                variant="outlined"
+                                type="email"
+                                error={errors.email ? true : false}
+                                helperText={errors.email ? 'Invalid email address' : ''}
+                                fullWidth
+                                margin="normal"
+                            />
 
-                <Button type="submit" variant="contained" color="primary" fullWidth>
-                    Create User
-                </Button>
-            </form>
-        </Container>
+                            <FormControl fullWidth margin="normal">
+                                <InputLabel id="role-select-label">Roles</InputLabel>
+                                <Select
+                                    labelId="role-select-label"
+                                    multiple
+                                    value={selectedRoles}
+                                    onChange={handleRoleChange}
+                                    renderValue={(selected) => selected.join(', ')}
+                                    MenuProps={{
+                                        PaperProps: {
+                                            style: {
+                                                maxHeight: 224,
+                                                width: 250,
+                                            },
+                                        },
+                                    }}
+                                >
+                                    {allRoles.map((role) => (
+                                        <MenuItem key={role} value={role}>
+                                            <Checkbox checked={selectedRoles.indexOf(role) > -1} />
+                                            <ListItemText primary={role} />
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+
+                            <Button type="submit" variant="contained" color="primary" fullWidth>
+                                Create User
+                            </Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
     );
 };
 
